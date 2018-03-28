5e85248e47c6d75231f727527c3655ac56ddd8d5
Josef Chmel <chmel@jchsoft.cz>
2018-03-28 17:15:33 +0200

    refactor aktualizace - předělání na class a o úroveň níže
    module Services
      class Deploy ...

--- main.rb
+++ main.rb
@@ -1,20 +1,23 @@
 require 'sinatra'
 require_relative 'app/config'
-require_relative 'services/deploy/default'
+require_relative 'services/deploy'
 require_relative 'lib/http_status'
 
 set :server, :puma
 
 post '/event_handler/:project' do
   $logger.info"for: #{params['project']}"
-  $logger.info'Hello world!'
+
   halt NOT_FOUND, "#{params['project']} not found!" unless $config[:projects][params['project'].to_sym]
   halt BAD_REQUEST, "payload in data not found!" unless params['payload']
+
   payload = JSON.parse(params['payload'])
   $logger.info "payload commit: #{payload['commit'].inspect}"
 
-  halt OK, 'not a circleCi success' unless Services::Deploy::Default.circle_ci_success?(payload)
-  halt OK, 'not a master branch' unless Services::Deploy::Default.master_branch?(payload['branches'])
+  deploy = Services::Deploy.new params['project'], payload
 
-  Services::Deploy::Default.update params['project'].to_sym, payload
+  halt OK, 'not a circleCi success' unless deploy.circle_ci_success?
+  halt OK, 'not a master branch' unless deploy.right_branch?
+
+  deploy.update!
 end
---
+++ services/deploy.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+require 'mail'
+
+module Services
+  class Deploy
+
+    class << self
+
+    end
+
+    # @param [String] project
+    # @param [Hash] payload from GitHub
+    def initialize(project, payload)
+      @project = project
+      @payload = payload
+    end
+
+    def circle_ci_success?
+      @payload['context'] == 'ci/circleci' && @payload['state'] == 'success'
+    end
+
+    def right_branch?
+      @payload['branches'].select { |k| k['name'] == $config[:projects][@project.to_sym][:branch] }.any?
+    end
+
+    def update!
+      Thread.new do
+        Dir.chdir($config[:projects][@project.to_sym][:path]) do
+          $logger.debug Dir.getwd
+          deploy_failed = false
+          $config[:projects][@project.to_sym][:commands].each do |command|
+            if command.key?('run')
+              $logger.debug `#{command['run']}`
+              if $?.exitstatus.to_i != 0
+                send_email failed_command: command['run'], exitstatus: $?.exitstatus
+                deploy_failed = true
+                break
+              end
+            end
+          end
+          send_email unless deploy_failed
+        end
+      end
+      'Deployment started!'
+    end
+
+    private
+
+    def send_email(failed_command: nil, exitstatus: nil)
+      subject_text = "Deployment of #{@payload['repository']['name']} #{failed_command ? 'failed on ' : 'was'} #{failed_command || 'successful'}#{" with exitstatus #{exitstatus}" if exitstatus}!"
+      author = @payload['commit']['commit']['author']['email']
+      commit = @payload['commit']
+      Mail.deliver do
+        from "notification@jchsoft.cz"
+        to author
+        subject subject_text
+        body JSON.pretty_generate(commit)
+      end
+    end
+
+  end
+end
